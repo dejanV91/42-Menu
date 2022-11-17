fce39176475bd80e6cda46d3a26c52a6d5466b8b
dejanV91 <dejanvukoicic91@gmail.com>
2022-11-17 09:41:30 +0100

    initial commit

--- src/shared/categoryItemsConfig.js
+++ src/shared/categoryItemsConfig.js
@@ -1,18 +1,5 @@
 import menu from "../data";
 
-// export const categories = menu
-//   .filter((item, index, self) => {
-//     return (
-//       self.findIndex(({ category }) => {
-//         return category === item.category;
-//       }) === index
-//     );
-//   })
-//   .map((item) => {
-//     return item.category;
-//   })
-//   .concat("all");
-
 export const categories = menu.reduce(
   (value, item) => {
     if (!value.includes(item.category)) {
