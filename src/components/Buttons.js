651955d4274f5c9cdb42df5239167fed11eb8453
dejanV91 <dejanvukoicic91@gmail.com>
2022-11-18 10:29:15 +0100

    initial commit

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,6 +1,7 @@
 function Buttons({ categories, filterItems }) {
-  return categories.map((buttonName) => (
+  return categories.map((buttonName, index) => (
     <button
+      key={index}
       type="button"
       name={buttonName}
       className="filter-btn"
